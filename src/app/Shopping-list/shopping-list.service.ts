3a5de30f8c8874fc2fb12618f2225f02297b65bb
tommy.adam@telekom.de <tommy.adam@telekom.de>
2023-07-21 11:45:32 +0200

    Fixing bugs

--- src/app/Shopping-list/shopping-list.service.ts
+++ src/app/Shopping-list/shopping-list.service.ts
@@ -11,24 +11,44 @@ export class ShoppingListService {
   // shopping list exists there
   mySubject$ = new BehaviorSubject<Recipe[]>([]);
 
+
   constructor() {
     const shoppingListData = localStorage.getItem('shoppingList');
+
 
     if (shoppingListData) {
       const parsedData = JSON.parse(shoppingListData);
       this.mySubject$.next(parsedData);
     }
 
+
     this.mySubject$.subscribe((value) => {
       console.log('New Shopping List:', value);
+
 
       // Storing in local storage
       localStorage.setItem('shoppingList', JSON.stringify(value));
     });
   }
 
+
   addNewRecipeToSL(recipe: Recipe) {
     console.log('New Shopping List Item:', recipe);
-    this.mySubject$.next([...this.mySubject$.getValue(), recipe]);
+    this.mySubject$.next(
+      this.mySubject$.getValue().concat(
+        // This madness is to ensure writability
+        // to items in the shopping list.
+        // Basically just creating a mutable copy.
+        JSON.parse(JSON.stringify(recipe))
+        // Other way of achieving that, dependant
+        // on our specific recipe structure:
+        // {
+        //   ...recipe,
+        //   ingredients: recipe.ingredients.map((ingredient) => ({
+        //     ...ingredient,
+        //   })),
+        // },
+      )
+    );
   }
 }
--- src/app/Shopping-list/shopping/shopping.component.ts
+++ src/app/Shopping-list/shopping/shopping.component.ts
@@ -14,42 +14,34 @@ export class ShoppingComponent implements OnInit {
   fav: Recipe[] = [];
   constructor(private shoppingService: ShoppingListService) {
     this.shoppingService.mySubject$.subscribe((res) => {
-      this.fav = res;
+      this.fav = [...res];
     });
   }
 
+
   ngOnInit() {}
   addNewRecipeToSL() {}
+
 
   deleteRecipe(recipe: Recipe) {
     const currentRecipes = this.shoppingService.mySubject$.getValue();
     const recipeIndex = currentRecipes.findIndex((r) => r === recipe);
 
+
     if (recipeIndex !== -1) {
       currentRecipes.splice(recipeIndex, 1);
-
-      localStorage.setItem('shoppingList', JSON.stringify(currentRecipes));
-
       this.shoppingService.mySubject$.next([...currentRecipes]);
     }
   }
 
+
   decrementQuantity(ingredient: Ingredient) {
     if (ingredient.quantity > 0) {
-      ingredient.quantity--;
-      localStorage.setItem('shoppingList', JSON.stringify(ingredient.quantity));
-  
-      this.shoppingService.mySubject$.next([...this.fav]);
-    }
+    ingredient.quantity--;
+    this.shoppingService.mySubject$.next(this.fav);}
   }
   incrementQuantity(ingredient: Ingredient) {
     ingredient.quantity++;
-    localStorage.setItem('shoppingList', JSON.stringify(this.fav));
-    this.shoppingService.mySubject$.next([...this.fav]);
+    this.shoppingService.mySubject$.next(this.fav);
   }
-
-  /*  updateLocalStorage() {
-    localStorage.setItem('shoppingList', JSON.stringify(this.fav));
-    this.shoppingService.mySubject$.next([...this.fav]); // Update the ShoppingListService with the new shopping list
-  } */
 }
