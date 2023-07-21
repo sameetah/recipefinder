a8c4e2277a0c19715e8c4dbff8fc05972cc868ee
Fatima <sameetah.fatima@telekom.de>
2023-07-21 10:17:01 +0200

    added some styles

--- src/app/Shopping-list/shopping/shopping.component.ts
+++ src/app/Shopping-list/shopping/shopping.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingListService } from '../shopping-list.service';
-import { Recipe } from 'src/app/recipe.model';
+import { Ingredient, Recipe } from 'src/app/recipe.model';
 import { Subscription } from 'rxjs';
 import { FavoritesComponent } from 'src/app/favorites/favorites.component';
 
@@ -10,6 +10,7 @@ import { FavoritesComponent } from 'src/app/favorites/favorites.component';
   styleUrls: ['./shopping.component.scss'],
 })
 export class ShoppingComponent implements OnInit {
+  quantity = 0;
   fav: Recipe[] = [];
   constructor(private shoppingService: ShoppingListService) {
     this.shoppingService.mySubject$.subscribe((res) => {
@@ -32,4 +33,21 @@ export class ShoppingComponent implements OnInit {
       this.shoppingService.mySubject$.next([...currentRecipes]);
     }
   }
+
+  decrementQuantity(ingredient: Ingredient) {
+    ingredient.quantity--;
+    localStorage.setItem('shoppingList', JSON.stringify(ingredient.quantity));
+
+    this.shoppingService.mySubject$.next([...this.fav]);
+  }
+  incrementQuantity(ingredient: Ingredient) {
+    ingredient.quantity++;
+    localStorage.setItem('shoppingList', JSON.stringify(this.fav));
+    this.shoppingService.mySubject$.next([...this.fav]);
+  }
+
+  /*  updateLocalStorage() {
+    localStorage.setItem('shoppingList', JSON.stringify(this.fav));
+    this.shoppingService.mySubject$.next([...this.fav]); // Update the ShoppingListService with the new shopping list
+  } */
 }
