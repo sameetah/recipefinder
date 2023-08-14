786c940d7cddd7146aa8ea070101b47892d31fc1
sevincdamla@gmail.com <sevincdamla@gmail.com>
2023-08-14 15:08:28 +0200

    some styling in contact form

--- src/app/components/contact-dialog-box/contact-dialog-box.component.ts
+++ src/app/components/contact-dialog-box/contact-dialog-box.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
-
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar'
 @Component({
   selector: 'app-contact-dialog-box',
   templateUrl: './contact-dialog-box.component.html',
@@ -11,22 +11,26 @@ import { ContactFormComponent } from '../contact-form/contact-form.component';
 export class ContactDialogBoxComponent {
   constructor(
     public dialogRef: MatDialogRef<ContactDialogBoxComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
   ) { }
+
 
   name: string = "";
   email: string = "";
   message: string = "";
+  isFormSubmitted: boolean = false
 
-  submitForm(){
+  submitForm() {
     if (this.name && this.email && this.message) {
       // Perform any desired actions with the form data
       // For example, you can pass the form data back to the ContactFormComponent:
-    this.dialogRef.close({
-      name: this.name,
-      email: this.email,
-      message: this.message
-    })
-    }
+      this.dialogRef.close({
+        name: this.name,
+        email: this.email,
+        message: this.message
+      });
+
+      // Set the flag to show the success message after a brief delay
+  }
   }
 }
